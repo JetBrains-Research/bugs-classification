17f0018d514ec2023d90f1a2a8a59aee8c9fa385
ArtyomLobanov <lobanov98@yandex.ru>
2018-07-03 13:20:28 +0300

    Minor changes

--- core/src/main/java/org/ml_methods_group/core/vectorization/Wrapper.java
+++ core/src/main/java/org/ml_methods_group/core/vectorization/Wrapper.java
@@ -1,15 +1,15 @@
-package org.ml_methods_group.clusterization;
+package org.ml_methods_group.core.vectorization;
 
-public class Wrap {
+public class Wrapper {
     public final double[] vector;
     public final int sessionId;
 
-    public Wrap(double[] vector, int sessionId) {
+    public Wrapper(double[] vector, int sessionId) {
         this.vector = vector;
         this.sessionId = sessionId;
     }
 
-    public static double distance(Wrap first, Wrap second) {
+    public static double distance(Wrapper first, Wrapper second) {
         double result = 0;
         for (int i = 0; i < first.vector.length; i++) {
             result += (first.vector[i] - second.vector[i]) * (first.vector[i] - second.vector[i]);

1c4e10d14b1a76e0d6e4134cb440e73567ac0102
ArtyomLobanov <lobanov98@yandex.ru>
2018-07-19 19:18:20 +0300

    Improve parallel framework

--- core/src/main/java/org/ml_methods_group/core/parallel/ParallelContext.java
+++ core/src/main/java/org/ml_methods_group/core/parallel/ParallelContext.java
@@ -20,11 +20,37 @@ public class ParallelContext implements AutoCloseable {
         service.shutdown();
     }
 
-    public <A, V> A runParallel(List<V> values, Supplier<A> accumulatorFactory,
-                                 BiFunction<V, A, A> processor, BinaryOperator<A> combiner) {
+    public <V, A> A runParallel(List<V> values,
+                                Supplier<A> accumulatorFactory,
+                                BiFunction<V, A, A> processor,
+                                BinaryOperator<A> combiner) {
+        return this.<V, Void, A>runParallel(values,
+                accumulatorFactory,
+                () -> null,
+                (value, context, accumulator) -> processor.apply(value, accumulator),
+                combiner);
+    }
+
+    public <V, C, A> A runParallel(List<V> values,
+                                   Supplier<A> accumulatorFactory,
+                                   C context,
+                                   ParallelProcessor<V, C, A> processor,
+                                   BinaryOperator<A> combiner) {
+        return this.<V, C, A>runParallel(values,
+                accumulatorFactory,
+                () -> context,
+                processor,
+                combiner);
+    }
+
+    public <V, C, A> A runParallel(List<V> values,
+                                   Supplier<A> accumulatorFactory,
+                                   Supplier<C> contextFactory,
+                                   ParallelProcessor<V, C, A> processor,
+                                   BinaryOperator<A> combiner) {
         final List<Callable<A>> tasks = splitValues(values).stream()
                 .sequential()
-                .map(list -> new Task<>(list, accumulatorFactory, processor))
+                .map(list -> new Task<>(list, accumulatorFactory, contextFactory, processor))
                 .collect(Collectors.toList());
         final List<Future<A>> results = new ArrayList<>();
         for (Callable<A> task : tasks) {
@@ -32,12 +58,12 @@ public class ParallelContext implements AutoCloseable {
         }
         return results.stream()
                 .sequential()
-                .map(this::getResult)
+                .map(ParallelContext::getResult)
                 .reduce(combiner)
                 .orElseGet(accumulatorFactory);
     }
 
-    private <V> List<List<V>> splitValues(List<V> values) {
+    private static <V> List<List<V>> splitValues(List<V> values) {
         final List<List<V>> lists = new ArrayList<>();
         final int valuesCount = values.size();
         final int blocksCount = Math.min(4, values.size());
@@ -48,7 +74,7 @@ public class ParallelContext implements AutoCloseable {
         return lists;
     }
 
-    private <V> V getResult(Future<V> future) {
+    private static <V> V getResult(Future<V> future) {
         while (true) {
             try {
                 return future.get();
@@ -59,21 +85,25 @@ public class ParallelContext implements AutoCloseable {
         }
     }
 
-    private class Task<A, V> implements Callable<A> {
+    private class Task<V, C, A> implements Callable<A> {
         private final List<V> values;
         private final Supplier<A> accumulatorFactory;
-        private final BiFunction<V, A, A> processor;
+        private final Supplier<C> contextFactory;
+        private final ParallelProcessor<V, C, A> processor;
 
-        private Task(List<V> values, Supplier<A> accumulatorFactory, BiFunction<V, A, A> processor) {
+        private Task(List<V> values, Supplier<A> accumulatorFactory, Supplier<C> contextFactory,
+                     ParallelProcessor<V, C, A> processor) {
             this.values = values;
             this.accumulatorFactory = accumulatorFactory;
+            this.contextFactory = contextFactory;
             this.processor = processor;
         }
 
         public A call() {
+            final C context = contextFactory.get();
             A accumulator = accumulatorFactory.get();
             for (V value : values) {
-                accumulator = processor.apply(value, accumulator);
+                accumulator = processor.process(value, context, accumulator);
             }
             return accumulator;
         }

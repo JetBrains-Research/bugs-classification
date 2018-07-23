dcc8d11e24d036b201e041f00288edbf9d6d79c9
ArtyomLobanov <lobanov98@yandex.ru>
2018-07-23 11:40:31 +0300

    Run solutions analisis parallel

--- nearest-correct/src/main/java/org/ml_methods_group/NearestCorrectStrategy.java
+++ nearest-correct/src/main/java/org/ml_methods_group/NearestCorrectStrategy.java
@@ -4,39 +4,54 @@ import org.ml_methods_group.core.*;
 import org.ml_methods_group.core.changes.ChangeGenerator;
 import org.ml_methods_group.core.entities.CodeChange;
 import org.ml_methods_group.core.entities.Solution;
+import org.ml_methods_group.core.parallel.ParallelContext;
+import org.ml_methods_group.core.parallel.ParallelUtils;
 import org.ml_methods_group.core.vectorization.EncodingStrategy;
 import org.ml_methods_group.core.vectorization.VectorTemplate;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-import static org.ml_methods_group.core.entities.Solution.Verdict.*;
+import static org.ml_methods_group.core.entities.Solution.Verdict.FAIL;
+import static org.ml_methods_group.core.entities.Solution.Verdict.OK;
 
 public class NearestCorrectStrategy implements AnalysisStrategy<Solution, double[]> {
-
-    private final Map<Long, Integer> codeCounters = new HashMap<>();
 
     private final List<Solution> correctSolutions = new ArrayList<>();
     private final List<Solution> incorrectSolutions = new ArrayList<>();
 
     private final ChangeGenerator generator;
     private final List<EncodingStrategy> strategies;
-    private final Clusterer<Solution> clusterer;
-    private final Selector<Solution> selector;
+    private final Supplier<List<Solution>> centersGenerator;
     private final DistanceFunction<Solution> metric;
     private final VectorTemplate.Postprocessor postprocessor;
     private FeaturesExtractor<Solution, double[]> extractor;
 
-    public NearestCorrectStrategy(ChangeGenerator generator, Clusterer<Solution> clusterer,
+    public NearestCorrectStrategy(ChangeGenerator generator,
+                                  Clusterer<Solution> clusterer,
                                   Selector<Solution> selector,
                                   DistanceFunction<Solution> metric,
                                   VectorTemplate.Postprocessor postprocessor,
                                   List<EncodingStrategy> strategies) {
         this.generator = generator;
-        this.selector = selector;
+        this.centersGenerator = () -> clusterer.buildClusters(correctSolutions).stream()
+                .map(selector::getCenter)
+                .collect(Collectors.toList());
         this.metric = metric;
         this.strategies = new ArrayList<>(strategies);
-        this.clusterer = clusterer;
+        this.postprocessor = postprocessor;
+    }
+
+    public NearestCorrectStrategy(ChangeGenerator generator,
+                                  DistanceFunction<Solution> metric,
+                                  VectorTemplate.Postprocessor postprocessor,
+                                  List<EncodingStrategy> strategies) {
+        this.generator = generator;
+        this.centersGenerator = () -> correctSolutions;
+        this.metric = metric;
+        this.strategies = new ArrayList<>(strategies);
         this.postprocessor = postprocessor;
     }
 
@@ -63,46 +78,50 @@ public class NearestCorrectStrategy implements AnalysisStrategy<Solution, double
         if (extractor != null) {
             return extractor;
         }
-        clear();
-        final List<Solution> centers =
-//                clusterer.buildClusters(correctSolutions).stream()
-//                .map(selector::getCenter)
-//                .collect(Collectors.toList());
-                correctSolutions;
+        final List<Solution> centers = centersGenerator.get();
         System.out.println("Centers: " + centers.size());
-        incorrectSolutions.forEach(solution -> index(solution, centers));
-        final List<Long> codes = codeCounters.entrySet().stream()
-                .filter(entry -> entry.getValue() > 1)
+        final Map<Integer, Solution> cache = new ConcurrentHashMap<>();
+        final Map<Long, Integer> counters;
+        try (ParallelContext context = new ParallelContext()) {
+            long s = System.currentTimeMillis();
+            counters = context.<Solution, Map<Long, Integer>>runParallel(incorrectSolutions, HashMap::new,
+                    (solution, accumulator) -> index(solution, centers, cache, accumulator),
+                    ParallelUtils::combineCounters);
+            System.out.println("Time: " + (System.currentTimeMillis() - s));
+        }
+        final List<Long> codes = counters.entrySet().stream()
+                .filter(entry -> entry.getValue() > 2)
                 .map(Map.Entry::getKey)
                 .collect(Collectors.toList());
-//        System.out.println("Codes: " + codes.size());
+        System.out.println("Codes: " + codes.size());
         final VectorTemplate template = new VectorTemplate(codes, postprocessor, strategies);
-        return new NearestCorrectFeaturesExtractor(template, centers, metric, generator);
+        return new NearestCorrectFeaturesExtractor(template, centers, cache, metric, generator);
     }
 
-    private void index(Solution solution, List<Solution> centers) {
+    private Map<Long, Integer> index(Solution solution, List<Solution> centers, Map<Integer, Solution> cache,
+                                     Map<Long, Integer> counters) {
         final Solution target = findNearest(solution, centers, metric);
+        cache.put(solution.getSolutionId(), target);
         final List<CodeChange> changes = generator.getChanges(solution, target);
         System.out.println("Changes: " + changes.size() + " " + (solution.getSessionId() == target.getSessionId()));
         for (EncodingStrategy strategy : strategies) {
             for (CodeChange change : changes) {
                 final long code = strategy.encode(change);
-                codeCounters.compute(code, (x, old) -> old == null ? 1 : 1 + old);
+                counters.compute(code, (x, old) -> old == null ? 1 : 1 + old);
             }
         }
+        return counters;
     }
 
     private void invalidate() {
         extractor = null;
     }
 
-    private void clear() {
-        codeCounters.clear();
-    }
-
     private static Solution findNearest(Solution solution, List<Solution> targets, DistanceFunction<Solution> metric) {
-        double currentMax = Integer.MAX_VALUE;
-        Solution best = null;
+        Solution best = targets.stream()
+                .filter(target -> target.getSessionId() == solution.getSessionId())
+                .findAny().orElse(targets.get(0));
+        double currentMax = metric.distance(solution, best);
         for (Solution target : targets) {
             final double currentDistance = metric.distance(solution, target, currentMax);
             if (currentDistance < currentMax) {
@@ -117,20 +136,24 @@ public class NearestCorrectStrategy implements AnalysisStrategy<Solution, double
 
         private final VectorTemplate template;
         private final List<Solution> centers;
+        private final Map<Integer, Solution> cache;
         private final DistanceFunction<Solution> metric;
         private final ChangeGenerator generator;
 
         private NearestCorrectFeaturesExtractor(VectorTemplate template, List<Solution> centers,
-                                                DistanceFunction<Solution> metric, ChangeGenerator generator) {
+                                                Map<Integer, Solution> cache, DistanceFunction<Solution> metric,
+                                                ChangeGenerator generator) {
             this.template = template;
             this.centers = centers;
+            this.cache = cache;
             this.metric = metric;
             this.generator = generator;
         }
 
         @Override
         public double[] process(Solution value) {
-            final Solution target = findNearest(value, centers, metric);
+            final Solution target = cache.computeIfAbsent(value.getSolutionId(),
+                    x -> findNearest(value, centers, metric));
             final List<CodeChange> changes = generator.getChanges(value, target);
             return template.process(changes);
         }

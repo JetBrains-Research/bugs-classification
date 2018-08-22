5bc77d26dead81620b035b41c2034975601a623b
ArtyomLobanov <lobanov98@yandex.ru>
2018-08-22 16:48:32 +0300

    Implement new predicate-based marker

---
+++ core/src/main/java/org/ml_methods_group/core/basic/markers/PredicateMarker.java
@@ -0,0 +1,23 @@
+package org.ml_methods_group.core.basic.markers;
+
+import org.ml_methods_group.core.Marker;
+
+import java.util.function.Predicate;
+
+public class PredicateMarker<V, M> implements Marker<V, M> {
+
+    private final Marker<V, M> oracle;
+    private final Predicate<V> predicate;
+    private final M mark;
+
+    public PredicateMarker(Predicate<V> predicate, M mark, Marker<V, M> oracle) {
+        this.oracle = oracle;
+        this.predicate = predicate;
+        this.mark = mark;
+    }
+
+    @Override
+    public M mark(V value) {
+        return predicate.test(value) ? mark : oracle.mark(value);
+    }
+}

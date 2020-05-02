a3242a9bd9612e9c2083a927fe00e2fdbc27ee21
SmirnovOleg <sm-oleg1209@yandex.ru>
2020-05-02 20:49:12 +0300

    Add FullDatasetEvaluation

--- src/main/java/org/ml_methods_group/evaluation/FullDatasetEvaluation.java
+++ src/main/java/org/ml_methods_group/evaluation/FullDatasetEvaluation.java
@@ -2,13 +2,18 @@ package org.ml_methods_group.evaluation;
 
 import org.ml_methods_group.common.Dataset;
 import org.ml_methods_group.common.Solution;
+import org.ml_methods_group.evaluation.preparation.ClustersCreator;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static org.ml_methods_group.common.Solution.Verdict.FAIL;
+import static org.ml_methods_group.common.Solution.Verdict.OK;
 import static org.ml_methods_group.common.serialization.ProtobufSerializationUtils.loadDataset;
 import static org.ml_methods_group.common.serialization.ProtobufSerializationUtils.storeDataset;
 
@@ -19,23 +24,32 @@ public class FullDatasetEvaluation {
         Set<Integer> problemIds = full.getValues().stream()
                 .map(Solution::getProblemId)
                 .collect(Collectors.toSet());
-        System.out.println("Total solutions: " + full.getValues().size());
-        System.out.println("Problems: " + problemIds.size());
-        problemIds.forEach(x -> System.out.print(x + " "));
 
+        List<String> problems = new ArrayList<>();
         problemIds.forEach(id -> {
-            List<Solution> solutions = full.getValues(x -> x.getProblemId() == id);
-            int ratio = (int) (solutions.size() * 0.8);
-            List<Solution> train = solutions.subList(0, ratio);
-            List<Solution> test = solutions.subList(ratio, solutions.size());
-            try {
-                Path pathToDataset = EvaluationInfo.PATH_TO_DATASET.resolve(id.toString());
-                storeDataset(new Dataset(train), pathToDataset.resolve("train.tmp"));
-                storeDataset(new Dataset(test), pathToDataset.resolve("test.tmp"));
-            } catch (IOException e) {
-                e.printStackTrace();
+            List<Solution> correct = full.getValues(x -> x.getProblemId() == id && x.getVerdict() == OK);
+            List<Solution> incorrect = full.getValues(x -> x.getProblemId() == id && x.getVerdict() == FAIL);
+            int ratio = (int) Math.round(incorrect.size() * 0.8);
+            List<Solution> trainIncorrect = incorrect.subList(0, ratio);
+            List<Solution> test = incorrect.subList(ratio, incorrect.size());
+            if (trainIncorrect.size() > 400) {
+                problems.add(id.toString());
+                List<Solution> train = Stream.concat(trainIncorrect.stream(), correct.stream())
+                        .collect(Collectors.toList());
+                System.out.println(train.size() + " - " + test.size());
+                try {
+                    Path pathToDataset = EvaluationInfo.PATH_TO_DATASET.resolve("full").resolve(id.toString());
+                    storeDataset(new Dataset(train), pathToDataset.resolve("train.tmp"));
+                    storeDataset(new Dataset(test), pathToDataset.resolve("test.tmp"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
+
+        System.out.println(problems.size());
+        ClustersCreator creator = new ClustersCreator();
+        creator.createGlobalClusters(problems.toArray(String[]::new), 0.3);
     }
 
 }

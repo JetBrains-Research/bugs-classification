f6a067126adb67e8380fdc83a6c6f6952917a27f
SmirnovOleg <sm-oleg1209@yandex.ru>
2020-05-05 06:46:52 +0300

    Add marking for global clusters

--- src/main/java/org/ml_methods_group/evaluation/FullDatasetEvaluation.java
+++ src/main/java/org/ml_methods_group/evaluation/FullDatasetEvaluation.java
@@ -2,10 +2,7 @@ package org.ml_methods_group.evaluation;
 
 import org.ml_methods_group.common.Dataset;
 import org.ml_methods_group.common.Solution;
-import org.ml_methods_group.evaluation.preparation.ClustersCreator;
 
-import java.io.IOException;
-import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
@@ -15,12 +12,11 @@ import java.util.stream.Stream;
 import static org.ml_methods_group.common.Solution.Verdict.FAIL;
 import static org.ml_methods_group.common.Solution.Verdict.OK;
 import static org.ml_methods_group.common.serialization.ProtobufSerializationUtils.loadDataset;
-import static org.ml_methods_group.common.serialization.ProtobufSerializationUtils.storeDataset;
 
 public class FullDatasetEvaluation {
 
     public static void main(String[] argv) throws Exception {
-        final Dataset full = loadDataset(EvaluationInfo.PATH_TO_DATASET.resolve("dataset2.tmp"));
+        final Dataset full = loadDataset(EvaluationInfo.PATH_TO_DATASET.resolve("dataset1.tmp"));
         Set<Integer> problemIds = full.getValues().stream()
                 .map(Solution::getProblemId)
                 .collect(Collectors.toSet());
@@ -37,19 +33,18 @@ public class FullDatasetEvaluation {
                 List<Solution> train = Stream.concat(trainIncorrect.stream(), correct.stream())
                         .collect(Collectors.toList());
                 System.out.println(train.size() + " - " + test.size());
-                try {
-                    Path pathToDataset = EvaluationInfo.PATH_TO_DATASET.resolve("full").resolve(id.toString());
-                    storeDataset(new Dataset(train), pathToDataset.resolve("train.tmp"));
-                    storeDataset(new Dataset(test), pathToDataset.resolve("test.tmp"));
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+//                try {
+//                    Path pathToDataset = EvaluationInfo.PATH_TO_DATASET.resolve("full").resolve(id.toString());
+//                    storeDataset(new Dataset(train), pathToDataset.resolve("train.tmp"));
+//                    storeDataset(new Dataset(test), pathToDataset.resolve("test.tmp"));
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
             }
         });
 
         System.out.println(problems.size());
-        ClustersCreator creator = new ClustersCreator();
-        creator.createGlobalClusters(problems.toArray(String[]::new), 0.3);
+        MarkingEvaluation.markGlobalClusters(problems);
     }
 
 }
--- src/main/java/org/ml_methods_group/evaluation/MarkingEvaluation.java
+++ src/main/java/org/ml_methods_group/evaluation/MarkingEvaluation.java
@@ -10,6 +10,7 @@ import org.ml_methods_group.common.ast.generation.ASTGenerator;
 import org.ml_methods_group.common.ast.generation.CachedASTGenerator;
 import org.ml_methods_group.common.ast.normalization.NamesASTNormalizer;
 import org.ml_methods_group.common.extractors.ChangesExtractor;
+import org.ml_methods_group.common.extractors.ManyProblemsBasedChangesExtractor;
 import org.ml_methods_group.common.metrics.functions.HeuristicChangesBasedDistanceFunction;
 import org.ml_methods_group.common.metrics.selectors.ClosestPairSelector;
 import org.ml_methods_group.common.preparation.basic.BasicUnifier;
@@ -19,11 +20,13 @@ import org.ml_methods_group.testing.selectors.CacheOptionSelector;
 
 import java.nio.file.Path;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import static org.ml_methods_group.common.Solution.Verdict.FAIL;
 import static org.ml_methods_group.common.Solution.Verdict.OK;
-import static org.ml_methods_group.common.serialization.ProtobufSerializationUtils.*;
+import static org.ml_methods_group.common.serialization.ProtobufSerializationUtils.loadDataset;
+import static org.ml_methods_group.common.serialization.ProtobufSerializationUtils.storeSolutionMarksHolder;
 
 public class MarkingEvaluation {
 
@@ -32,31 +35,92 @@ public class MarkingEvaluation {
     private final static Integer NUM_EXAMPLES = 10;
 
     public static void main(String[] argv) throws Exception {
-        final var marks = loadSolutionMarksHolder(PATH_TO_DATASET.resolve("test_marks.tmp"));
-        final Map<Solution, List<String>> fixedMarks = new HashMap<>();
-        try (Scanner scanner = new Scanner(System.in)) {
-            for (Map.Entry<Solution, List<String>> entry : marks) {
-                List<String> newMarks = new ArrayList<>();
-                entry.getValue().forEach(mark -> {
-                    String current = mark;
-                    if (mark.equals("not_all_catched"))
-                        current = "null_input";
-                    else if (mark.equals("next_int"))
-                        current = "wrong_read";
-                    System.out.println(current);
-                    String newMark = scanner.nextLine();
-                    if (newMark.equals("+"))
-                        newMarks.add(current);
-                    else
-                        newMarks.add(newMark);
-                });
-                fixedMarks.put(entry.getKey(), newMarks);
-            }
-            storeSolutionMarksHolder(
-                    new SolutionMarksHolder(fixedMarks),
-                    PATH_TO_DATASET.resolve("test_marks_fixed.tmp")
+    }
+
+    public static void markGlobalClusters(List<String> problems) throws Exception {
+        final var database = new HashDatabase(EvaluationInfo.PATH_TO_CACHE);
+        final ASTGenerator astGenerator = new CachedASTGenerator(new NamesASTNormalizer());
+        final var changeGenerator = new BasicChangeGenerator(astGenerator);
+        final var unifier = new BasicUnifier<>(
+                CommonUtils.compose(astGenerator::buildTree, ITree::getHash)::apply,
+                CommonUtils.checkEquals(astGenerator::buildTree, ASTUtils::deepEquals),
+                new MinValuePicker<>(Comparator.comparingInt(Solution::getSolutionId)));
+        final var metric = new HeuristicChangesBasedDistanceFunction(changeGenerator);
+        final var generatorByProblemId = new HashMap<Integer, FeaturesExtractor<Solution, Changes>>();
+
+        for (String problem : problems) {
+            final Dataset train = loadDataset(EvaluationInfo.PATH_TO_DATASET
+                    .resolve("full").resolve(problem).resolve("train.tmp"));
+            final List<Solution> correct = train.getValues(x -> x.getVerdict() == OK);
+            final var selector = new CacheOptionSelector<>(
+                    new ClosestPairSelector<>(unifier.unify(correct), metric),
+                    database,
+                    Solution::getSolutionId,
+                    Solution::getSolutionId
             );
+            int problemId = train.getValues().get(0).getProblemId();
+            generatorByProblemId.put(problemId, new ChangesExtractor(changeGenerator, selector));
         }
+        final var generator = new ManyProblemsBasedChangesExtractor(generatorByProblemId);
+        final var clusters = ProtobufSerializationUtils
+                .loadSolutionClusters(EvaluationInfo.PATH_TO_CLUSTERS.resolve("global_clusters.tmp"))
+                .getClusters().stream()
+                .sorted(Comparator.<Cluster<Solution>>comparingInt(Cluster::size).reversed())
+                .collect(Collectors.toList());
+
+        final var marks = new HashMap<Cluster<Solution>, String>();
+        try (Scanner scanner = new Scanner(System.in)) {
+            for (var cluster : clusters.subList(0, Math.min(clusters.size(), NUM_CLUSTERS))) {
+                System.out.println("Next cluster (size=" + cluster.size() + "):");
+                final List<Solution> solutions = cluster.elementsCopy();
+                Collections.shuffle(solutions);
+                final var problemCounter = solutions.stream()
+                        .map(Solution::getProblemId)
+                        .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+                System.out.println("Problems in cluster: ");
+                problemCounter.forEach((k, v) -> System.out.println(k + " - " + v));
+                System.out.println();
+                int start = 0;
+                while (true) {
+                    System.out.println("More?");
+                    if (scanner.nextLine().equals("+")) {
+                        int tmp = start;
+                        for (int i = start; i < Math.min(start + NUM_EXAMPLES, solutions.size()); i++) {
+                            final var solution = solutions.get(i);
+                            System.out.println("    Example #" + i);
+                            System.out.println("    Session id: " + solution.getSessionId());
+                            System.out.println(solution.getCode());
+                            System.out.println();
+                            System.out.println("    Submission fix:");
+                            generator.process(solution).getChanges().forEach(System.out::println);
+                            System.out.println();
+                            tmp++;
+                        }
+                        start = tmp;
+                    } else {
+                        break;
+                    }
+                }
+                System.out.println("---------------------------------------------------------------------------------");
+                System.out.println("Your mark:");
+                while (true) {
+                    final String mark = scanner.nextLine();
+                    if (mark.equals("-")) {
+                        marks.remove(cluster);
+                    } else if (mark.equals("+")) {
+                        System.out.println("Final mark: " + marks.get(cluster));
+                        break;
+                    } else {
+                        marks.put(cluster, mark);
+                    }
+                }
+            }
+        }
+        final var marked = new MarkedClusters<>(marks);
+        ProtobufSerializationUtils.storeMarkedClusters(
+                marked,
+                EvaluationInfo.PATH_TO_CLUSTERS.resolve("marked_global_clusters.tmp")
+        );
     }
 
     public static void markClusters() throws Exception {

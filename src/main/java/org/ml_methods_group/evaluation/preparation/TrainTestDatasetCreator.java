f4695842630c6449645a766c2edcb33ba7a83af0
SmirnovOleg <sm-oleg1209@yandex.ru>
2020-05-11 19:40:30 +0300

    Fix train-test separation

--- src/main/java/org/ml_methods_group/evaluation/preparation/TrainTestDatasetCreator.java
+++ src/main/java/org/ml_methods_group/evaluation/preparation/TrainTestDatasetCreator.java
@@ -2,13 +2,9 @@ package org.ml_methods_group.evaluation.preparation;
 
 import org.ml_methods_group.common.Dataset;
 import org.ml_methods_group.common.Solution;
-import org.ml_methods_group.common.serialization.ProtobufSerializationUtils;
 import org.ml_methods_group.evaluation.EvaluationInfo;
-import org.ml_methods_group.parsing.JavaCodeValidator;
-import org.ml_methods_group.parsing.ParsingUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -27,32 +23,29 @@ public class TrainTestDatasetCreator {
 
         final Random random = new Random(seed);
         final Dataset full = loadDataset(EvaluationInfo.PATH_TO_DATASET.resolve("dataset2.tmp"));
-        Set<Integer> problemIds = full.getValues().stream()
-                .map(Solution::getProblemId)
-                .collect(Collectors.toSet());
-
         List<Solution> solutions = full.getValues(x -> x.getProblemId() == problemId);
         Map<Integer, List<Solution>> sessionById = new HashMap<>();
         for (Solution solution : solutions) {
-            sessionById.computeIfAbsent(solution.getSessionId(), x -> new ArrayList<>())
-                    .add(solution);
+            sessionById.computeIfAbsent(solution.getSessionId(), x -> new ArrayList<>()).add(solution);
         }
         List<List<Solution>> sessions = new ArrayList<>(sessionById.values());
         Collections.shuffle(sessions, random);
         List<Solution> train = new ArrayList<>();
         List<Solution> test = new ArrayList<>();
         for (var session : sessions) {
-            if (session.size() == 1) continue;
-            Solution correct = session.stream().filter(x -> x.getVerdict() == OK)
-                    .collect(Collectors.toList()).get(0);
-            Solution incorrect = session.stream().filter(x -> x.getVerdict() == FAIL)
-                    .collect(Collectors.toList()).get(0);
-            if (test.size() < 2 * testSize) {
-                test.add(incorrect);
-                test.add(correct);
+            List<Solution> correct = session.stream()
+                    .filter(x -> x.getVerdict() == OK)
+                    .collect(Collectors.toList());
+            List<Solution> incorrect = session.stream()
+                    .filter(x -> x.getVerdict() == FAIL)
+                    .collect(Collectors.toList());
+            long currentTestSize = test.stream().filter(x -> x.getVerdict() == FAIL).count();
+            if (currentTestSize < testSize && !incorrect.isEmpty()) {
+                test.addAll(incorrect);
+                test.addAll(correct);
             } else {
-                train.add(incorrect);
-                train.add(correct);
+                train.addAll(incorrect);
+                train.addAll(correct);
             }
         }
 
